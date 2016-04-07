5824c9186e2d6ecf53db9ab06f489db787ba6677
Netai Nayek <net.nayek@gmail.com>
2016-04-08 00:21:48 +0530

    this is test
    need for git setup

--- controllers/siteController.js
+++ controllers/siteController.js
@@ -14,10 +14,12 @@ module.exports = {
 
       if(model){
         response = {
-          id: model.id,
-          user_type: model.get('user_type'),
-          name: model.get('name'),
-          email: model.get('email'),
+          data: {
+            id: model.id,
+            user_type: model.get('user_type'),
+            name: model.get('name'),
+            email: model.get('email'),
+          },
           token: tokenHelper.createToken(model),
           message: 'login successfull',
           status: 'success',
@@ -40,31 +42,10 @@ module.exports = {
   // POST /signup
   signup: function(req, res, next) {
 
-    usersModel.forge({email: email,password: password})
-    .fetch()
+    usersModel.forge(req.body)
+    .save()
     .then(function (model) {
-      response = {};
-
-      if(model){
-        response = {
-          id: model.id,
-          user_type: model.get('user_type'),
-          name: model.get('name'),
-          email: model.get('email'),
-          token: tokenHelper.createToken(model),
-          message: 'login successfull',
-          status: 'success',
-          code: '1001'
-        };
-      } else {
-        response = {
-          message: 'user not exist',
-          status: 'error',
-          code: '2001'
-        };
-      }
-
-      res.json(response);
+      res.json(model.toJSON());
     })
     .otherwise(function (error) {
       res.status(500).json({msg: error.message,code: 'SYSERR'});
